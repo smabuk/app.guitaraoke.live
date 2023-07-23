88e621ab9518f5f0e7799cfb145e1375c8f07774
smabuk <2011834+smabuk@users.noreply.github.com>
2023-07-23 11:13:36 +0100

    Add WebAssembly package

--- GuitaraokeBlazorApp/Program.cs
+++ GuitaraokeBlazorApp/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-	.AddServerComponents();
+	.AddServerComponents()
+	.AddWebAssemblyComponents();
 
 builder.Services.AddHttpClient();
 
